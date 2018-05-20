fc5cc1ebafdc886a89e8730e9655cb1296b92846
Roodrallec <jorge@maiv.co.uk>
2018-05-20 19:05:35 +0200

    calling chunker

--- sem_eval_part_1.py
+++ sem_eval_part_1.py
@@ -1,12 +1,13 @@
 import re
 import os
+from drug_entity_chunker import DrugEntityChunker
 from nltk import word_tokenize, pos_tag, ne_chunk
 from nltk.chunk import conlltags2tree, tree2conlltags
 
 TRAIN_X_DIR = './Train/DrugBank'
 TRAIN_Y_DIR = './Train/DrugBankOutput'
 
-TEST_X_DIR = './Test/DrugBank'
+TEST_X_DIR = './Test/Test for DDI Extraction task/DrugBank'
 TEST_Y_DIR = './Test/DrugBankOutput'
 
 
@@ -36,8 +37,8 @@ def sentence_text_from_doc(doc):
     return re.findall(regex, doc)
 
 
-def is_drug_bank_filename(file_name):
-    return file_name[-8:] == '_ddi.xml'
+def is_drug_bank_filename(path):
+    return path.count('DrugBank') > 0
 
 
 def pos_bio_tagger(text, drug_positions):
@@ -63,35 +64,28 @@ def pos_bio_tagger(text, drug_positions):
 
         extended_pos.append((word, pos, bio))
 
-
     return extended_pos
 
 
 def parse_drug_xml(xml_dir):
-
     for root, dirs, files in os.walk(xml_dir):
         for file_name in files:
             # TODO: add handling of Med Line dataset
-            if is_drug_bank_filename(file_name):
-                doc = open(os.path.join(root, file_name), 'rb').read().decode('utf-8')
+            path = os.path.join(root, file_name)
+            if is_drug_bank_filename(path):
+                doc = open(path, 'rb').read().decode('utf-8')
                 sentences = sentence_from_doc(doc)
 
                 for sentence in sentences:
                     text = text_from_sentence(sentence)
                     drug_positions = drug_pos_from_sentence(sentence)
                     tagged_text = pos_bio_tagger(text, drug_positions)
-                    print(tagged_text)
-                    # # print(ner)
-                    # iob_ner = tree2conlltags(ner)
-                    # print(iob_ner, '\n')
+                    yield [((word, pos), bio) for word, pos, bio in tagged_text]
 
 
-
-
-    return
-
-
-print('Evaluating SEM-EVAL part 1...')
-parse_drug_xml(TRAIN_X_DIR)
-# x_train, y_train, x_test, y_test = parse_med_xml(TRAIN_DIR), parse_med_xml(TEST_DIR)
-# classifier = load_classifier()
+train = list(parse_drug_xml(TRAIN_X_DIR))
+# test = list(parse_drug_xml(TEST_X_DIR))
+print(f'training on {len(train)} samples')
+# print(f'testing on {len(test)} samples')
+chunker = DrugEntityChunker(train)
+print(chunker.parse(pos_tag(word_tokenize("I'm going to Germany this Monday."))))
