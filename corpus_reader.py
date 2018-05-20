5d1456a0dc02d0b3fda00122492118ed6e666879
Roodrallec <jorge@maiv.co.uk>
2018-05-20 18:31:55 +0200

    corpus building done

---
+++ corpus_reader.py
@@ -0,0 +1,59 @@
+import os
+import collections
+corpus_root = "./corpus/gmb-2.2.0"   # Make sure you set the proper path to the unzipped corpus
+ner_tags = collections.Counter()
+
+
+def to_conll_iob(annotated_sentence):
+    """
+    `annotated_sentence` = list of triplets [(w1, t1, iob1), ...]
+    Transform a pseudo-IOB notation: O, PERSON, PERSON, O, O, LOCATION, O
+    to proper IOB notation: O, B-PERSON, I-PERSON, O, O, B-LOCATION, O
+    """
+    proper_iob_tokens = []
+    for idx, annotated_token in enumerate(annotated_sentence):
+        tag, word, ner = annotated_token
+
+        if ner != 'O':
+            if idx == 0:
+                ner = "B-" + ner
+            elif annotated_sentence[idx - 1][2] == ner:
+                ner = "I-" + ner
+            else:
+                ner = "B-" + ner
+        proper_iob_tokens.append((tag, word, ner))
+    return proper_iob_tokens
+
+
+def read_gmb(corpus_root):
+    for root, dirs, files in os.walk(corpus_root):
+        for filename in files:
+            if filename.endswith(".tags"):
+                with open(os.path.join(root, filename), 'rb') as file_handle:
+                    file_content = file_handle.read().decode('utf-8').strip()
+                    annotated_sentences = file_content.split('\n\n')
+                    for annotated_sentence in annotated_sentences:
+                        annotated_tokens = [seq for seq in annotated_sentence.split('\n') if seq]
+
+                        standard_form_tokens = []
+
+                        for idx, annotated_token in enumerate(annotated_tokens):
+                            annotations = annotated_token.split('\t')
+                            word, tag, ner = annotations[0], annotations[1], annotations[3]
+
+                            if ner != 'O':
+                                ner = ner.split('-')[0]
+
+                            if tag in ('LQU', 'RQU'):   # Make it NLTK compatible
+                                tag = "``"
+
+                            standard_form_tokens.append((word, tag, ner))
+
+                        conll_tokens = to_conll_iob(standard_form_tokens)
+
+                        # Make it NLTK Classifier compatible - [(w1, t1, iob1), ...] to [((w1, t1), iob1), ...]
+                        # Because the classfier expects a tuple as input, first item input, second the class
+                        print([((w, t), iob) for w, t, iob in conll_tokens])
+
+
+reader = read_gmb(corpus_root)
--- sem_eval_part_1.py
+++ sem_eval_part_1.py
@@ -1,7 +1,7 @@
 import re
-from os import listdir
-from os.path import isfile, join
+import os
 from nltk import word_tokenize, pos_tag, ne_chunk
+from nltk.chunk import conlltags2tree, tree2conlltags
 
 TRAIN_X_DIR = './Train/DrugBank'
 TRAIN_Y_DIR = './Train/DrugBankOutput'
@@ -10,13 +10,24 @@ TEST_X_DIR = './Test/DrugBank'
 TEST_Y_DIR = './Test/DrugBankOutput'
 
 
-def extract_sentences(doc):
-    regex = r'<sentence.*?text=".*?">'
-    return re.findall(regex, doc)
+def sentence_from_doc(doc):
+    doc = doc.replace('\n', '').replace('</document>', '')
+    sentences = doc.split('<sentence')[1:]
+    return ['<sentence' + str for str in sentences]
 
 
-def extract_text(sentence):
-    regex = r' text="(.*?)">'
+def text_from_sentence(sentence):
+    regex = r'<sentence .*?text="(.*?)".*?>'
+    return re.findall(regex, sentence)[0]
+
+
+def drug_pos_type_from_sentence(sentence):
+    regex = r'charOffset="(.*?)".*?type="(.*?)"'
+    return re.findall(regex, sentence)
+
+
+def drug_pos_from_sentence(sentence):
+    regex = r'<entity.*?charOffset="(.*?)".*?type="(.*?)"'
     return re.findall(regex, sentence)
 
 
@@ -25,33 +36,62 @@ def sentence_text_from_doc(doc):
     return re.findall(regex, doc)
 
 
-def parse_drug_xml(xml_dir, limit=None):
-    reached = 0
-
-    for f in listdir(xml_dir):
-
-        if limit is not None and limit == reached:
-            break
-
-        path = join(xml_dir, f)
-
-        if not isfile(path):
-            continue
-
-        doc = open(path, 'r').read()
-        texts = sentence_text_from_doc(doc)
-        ner = [ne_chunk(pos_tag(word_tokenize(text))) for text in texts]
-        print(ner)
+def is_drug_bank_filename(file_name):
+    return file_name[-8:] == '_ddi.xml'
 
 
+def pos_bio_tagger(text, drug_positions):
+    text = text.replace('-', ' ')
+    pos_tags = pos_tag(word_tokenize(text))
+    extended_pos = []
+    find_offset = 0
+
+    for word, pos in pos_tags:
+        bio = "O"
+        word_start = text.find(word, find_offset)
+        find_offset += text.count(' ', find_offset, find_offset + len(word)) + len(word)
+
+        for drug_pos_str, drug_type in drug_positions:
+            drug_pos_arr = drug_pos_str.split('-')  # expect '100-121' or '100-110;123-130'
+            drug_start, drug_end = int(drug_pos_arr[0]), int(drug_pos_arr[-1]) + 1
+
+            if word_start == drug_start:
+                bio = f'B-{drug_type}'
+
+            if drug_start < word_start < drug_end:
+                bio = f'I-{drug_type}'
+
+        extended_pos.append((word, pos, bio))
 
 
-        reached += 1
+    return extended_pos
+
+
+def parse_drug_xml(xml_dir):
+
+    for root, dirs, files in os.walk(xml_dir):
+        for file_name in files:
+            # TODO: add handling of Med Line dataset
+            if is_drug_bank_filename(file_name):
+                doc = open(os.path.join(root, file_name), 'rb').read().decode('utf-8')
+                sentences = sentence_from_doc(doc)
+
+                for sentence in sentences:
+                    text = text_from_sentence(sentence)
+                    drug_positions = drug_pos_from_sentence(sentence)
+                    tagged_text = pos_bio_tagger(text, drug_positions)
+                    print(tagged_text)
+                    # # print(ner)
+                    # iob_ner = tree2conlltags(ner)
+                    # print(iob_ner, '\n')
+
+
+
 
     return
 
 
 print('Evaluating SEM-EVAL part 1...')
-parse_drug_xml(TRAIN_X_DIR, 5)
+parse_drug_xml(TRAIN_X_DIR)
 # x_train, y_train, x_test, y_test = parse_med_xml(TRAIN_DIR), parse_med_xml(TEST_DIR)
 # classifier = load_classifier()
